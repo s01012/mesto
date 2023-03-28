44c36b37c43f4cb4da88de925208ca43e889574b
s01012 <hackimov.el@yandex.ru>
2023-03-28 18:04:09 +0300

    редактирование js замена let на const

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,49 @@
-let popupElement = document.querySelector('#editProfile');
-let popupElementCards = document.querySelector('#addCards');
-let popupImage = document.querySelector('#imageOpen');
-let personName = document.querySelector('.profile__title');
-let personDescription = document.querySelector('.profile__subtitle');
+const popupElement = document.querySelector('#editProfile');
+const popupElementCards = document.querySelector('#addCards');
+const popupImage = document.querySelector('#imageOpen');
+const personName = document.querySelector('.profile__title');
+const personDescription = document.querySelector('.profile__subtitle');
 
-let hello = document.querySelectorAll('.section_hello');
-console.log(hello);
+
 
 
 
 /*найти, положить элемент кнопки редактирования профиля в переменную, доабвить к нему слушатели и вызвать функцию*/
-let elementPen = document.querySelector('.profile__button-editor');
+const elementPen = document.querySelector('.profile__button-editor');
 elementPen.addEventListener('click', openPopup);
 
 /*найти, положить элемент кнопки ЗАКРЫТИЯ редактирования профиля в переменную, доабвить к нему слушатели и вызвать функцию*/
-let popupExit = document.querySelector('.popup__exit');
+const popupExit = document.querySelector('.popup__exit');
 popupExit.addEventListener('click', closePopup);
 
 /*найти, положить элемент кнопки ЗАКРЫТИЯ редактирования профиля в переменную, доабвить к нему слушатели при отправке формы,
 и вызвать функцию*/
-let formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form');
 formElement.addEventListener('submit', handleFormSubmit);
 
 /*переменные, которые находят в dom элементы с соответствующими id */
-let inputName = document.getElementById('name');
-let inputJob = document.getElementById('job');
+const inputName = document.getElementById('name');
+const inputJob = document.getElementById('job');
 
 /*найти, положить элемент кнопки добавления фотокарточек в переменную, доабвить к нему слушатели и вызвать функцию*/
-let elementPlus = document.querySelector('.profile__button-addition');
+const elementPlus = document.querySelector('.profile__button-addition');
 elementPlus.addEventListener('click', openPopupCards);
 
-let popupExitAddCards = popupElementCards.querySelector('.popup__exit');
+const popupExitAddCards = popupElementCards.querySelector('.popup__exit');
 popupExitAddCards.addEventListener('click', closePopupCards);
 
-let formElementAddCards = popupElementCards.querySelector('.popup__form');
+const formElementAddCards = popupElementCards.querySelector('.popup__form');
 formElementAddCards.addEventListener('submit', addCards);
 
 
-let popupImgLink = document.querySelector('.popup__image');
+const popupImgLink = document.querySelector('.popup__image');
 
-let popupImageExit = popupImage.querySelector('.popup__exit');
+const popupImageExit = popupImage.querySelector('.popup__exit');
 popupImageExit.addEventListener('click', closePopupImage);
 
 
-let inputTitle = document.getElementById('title');
-let inputLink = document.getElementById('link');
+const inputTitle = document.getElementById('title');
+const inputLink = document.getElementById('link');
 
 /* вытаскиваем содержимое шаблона и контейнер для карточек*/
 const templateContent = document.querySelector('#card').content;
@@ -80,7 +79,7 @@ const initialCards = [
 
 
   for(let i = 0; i<initialCards.length; i++){
-    let card = templateContent.querySelector('.elements__card').cloneNode(true);
+    const card = templateContent.querySelector('.elements__card').cloneNode(true);
     card.querySelector('.elements__card-image').setAttribute('src', initialCards[i].link);
     card.querySelector('.elements__card-image').setAttribute('alt', initialCards[i].name);
     card.querySelector('.elements__card-title').textContent = initialCards[i].name;
@@ -94,7 +93,7 @@ const initialCards = [
 
 
 function addCards() {
-  let card = templateContent.querySelector('.elements__card').cloneNode(true);
+  const card = templateContent.querySelector('.elements__card').cloneNode(true);
   card.querySelector('.elements__card-image').setAttribute('src', inputLink.value);
   card.querySelector('.elements__card-image').setAttribute('alt', inputTitle.value);
   card.querySelector('.elements__card-title').textContent = inputTitle.value;
@@ -154,11 +153,11 @@ function deleteCards(evt) {
 
 
 function openImage (evt) {
-  let cardImage = evt.target.getAttribute('src');
-  let cardTitle = evt.target.closest('.elements__card').querySelector('.elements__card-title').textContent;
+  const cardImage = evt.target.getAttribute('src');
+  const cardTitle = evt.target.closest('.elements__card').querySelector('.elements__card-title').textContent;
   popupImgLink.setAttribute('src', cardImage);
   popupImgLink.setAttribute('alt', cardTitle);
-  let popupCaption = popupImage.querySelector('.popup__caption');
+  const popupCaption = popupImage.querySelector('.popup__caption');
   popupCaption.textContent = cardTitle;
   popupImage.classList.add('popup_opened');
   popupImage.classList.add('popup_dark');
