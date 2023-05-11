a3868c344524a2677789194bcbc1e6b30348ef9a
s01012 <hackimov.el@yandex.ru>
2023-05-11 11:33:10 +0300

    add class formValidatior

---
+++ scripts/FormValidator.js
@@ -0,0 +1,96 @@
+class FormValidator {
+  constructor(validationConfig, formElement) {
+    this._formSelector         = validationConfig.formSelector;
+    this._inputSelector        = validationConfig.inputSelector;
+    this._submitButtonSelector = validationConfig.submitButtonSelector;
+    this._inactiveButtonClass  = validationConfig.inactiveButtonClass;
+    this._inputErrorClass      = validationConfig.inputErrorClass;
+    this._errorClass           = validationConfig.errorClass;
+    this._form = formElement;
+  }
+
+  // Передадим текст ошибки вторым параметром
+  _showInputError = (someInput, errorMessage) => {
+    someInput.classList.add(this._inputErrorClass);
+    const someError = document.querySelector(`.${someInput.id}-error`);
+    // Заменим содержимое span с ошибкой на переданный параметр
+    someError.textContent = errorMessage;
+    someError.classList.add(this._errorClass);
+  }
+
+  _hideInputError = (someInput) => {
+    someInput.classList.remove(this._inputErrorClass);
+    const someError = document.querySelector(`.${someInput.id}-error`);
+    someError.classList.remove(this._errorClass);
+    // Очистим ошибку
+    someError.textContent = '';
+  }
+
+  _isValid = (someInput) => { /*вызываем фнукцию, которая получает в качестве параметра поле ввода */
+
+    if (!someInput.validity.valid) { /*проверка, если у объекта validity значение false то вызовем функцию  showInputError*/
+      // Передадим сообщение об ошибке вторым аргументом
+      this._showInputError(someInput, someInput.validationMessage);
+    } else {                                                          // иначе hideInputError
+      this._hideInputError(someInput);
+    }
+  }
+
+  _hasInvalidInput = (allInputs) => {
+    return allInputs.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  };
+
+  _toggleSubmitButton = (allInputs, submitButton) => {
+    if (this._hasInvalidInput(allInputs)) {
+      submitButton.classList.add(this._inactiveButtonClass);
+      submitButton.setAttribute('disabled', true);
+    }
+    else {
+      submitButton.classList.remove(this._inactiveButtonClass);
+      submitButton.removeAttribute('disabled');
+    }
+  }
+
+
+  _addEventListeners = (someForm) => { /*вызываем фнукцию, которая получает в качестве параметра форму */
+    const allInputs = Array.from(someForm.querySelectorAll(this._inputSelector)); /*создали переменную, которая получает массив из всех полей ввода */
+
+    const submit = someForm.querySelector(this._submitButtonSelector);
+    this._toggleSubmitButton(allInputs, submit);
+    allInputs.forEach(element => { /*обходим все поля ввода */
+      element.addEventListener('input', () => {  /*на каждое поле ставим слушатель по вводлу клавиатуры */
+      this._isValid(element); /*вызвали функцию, передали поле ввода */
+
+        this._toggleSubmitButton(allInputs, submit);
+
+      })
+    });
+    someForm.addEventListener('submit', () => {
+      this._toggleSubmitButton(allInputs, submit);
+    })
+  }
+
+  enableValidation = () => { /* создали функцию  */
+
+    const allForms = Array.from(document.querySelectorAll(this._formSelector)); /* создали переменную. внутри переменной разместили массив состоящий из фвсех форм на сайте  */
+    allForms.forEach(form => { /* обходим все формы  */
+      this._addEventListeners(form); /*вызываем фнукцию, которой передаем каждый элемент массива состоящий из форм  */
+    });
+  }
+
+}
+
+
+const validationConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__sumbit',
+  inactiveButtonClass: 'form__sumbit_inactive',
+  inputErrorClass: 'form__input_error_border',
+  errorClass: 'form__input_error_active'
+}
+
+const vi = new FormValidator(validationConfig, 'pop');
+vi.enableValidation();
